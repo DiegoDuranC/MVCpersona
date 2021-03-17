f415f4143d93810509c3665f629221acabf7ea80
Diego Leonardo Duran Castaños <a2019119811@estudiantes.upsa.edu.bo>
2021-03-16 22:29:48 -0400

    Added Connection Strings

---
+++ Persona/api_Persona/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace api_Persona.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<api_Persona.Models.Person> People { get; set; }
+    }
+}
---
+++ Persona/api_Persona/Models/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace api_Persona.Models
+{
+    public enum SexType
+    {
+        Female = 0,
+        Male = 1
+    }
+
+    public class Person
+    {
+        [Key]
+        public int PersonID { get; set; }
+        [Required]
+        [StringLength(250, MinimumLength = 5, ErrorMessage = "Please enter a name from 5 to 50 characters lenght")]
+        public string Name { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime BirthDate { get; set; }
+        public SexType Sex { get; set; }
+
+
+    }
+}
